833e9b04aee67b252f0b196d5280b0ce395398d1
Alex J. Champandard <alexjc@aigamedev.com>
2016-03-06 16:47:23 +0100

    Adding a concatenation layer after pooling for the semantic map.

--- doodle.py
+++ doodle.py
@@ -1,11 +1,11 @@
 import pickle
-import collections
 
+import numpy as np
 import theano.tensor as T
 
 import lasagne
 from lasagne.layers import Conv2DLayer as ConvLayer, Pool2DLayer as PoolLayer
-from lasagne.layers import InputLayer
+from lasagne.layers import InputLayer, ConcatLayer
 
 
 class Model(object):
@@ -17,9 +17,11 @@ class Model(object):
         self.load_params()
 
     def build_model(self):
-        net = collections.OrderedDict()
-        net['input']   = InputLayer((1, 3, None, None))
-        net['conv1_1'] = ConvLayer(net['input'],   64, 3, pad=1)
+        net = {}
+
+        # Main network for the primary image.        
+        net['img']   = InputLayer((1, 3, None, None))
+        net['conv1_1'] = ConvLayer(net['img'],   64, 3, pad=1)
         net['conv1_2'] = ConvLayer(net['conv1_1'], 64, 3, pad=1)
         net['pool1']   = PoolLayer(net['conv1_2'], 2, mode='average_exc_pad')
         net['conv2_1'] = ConvLayer(net['pool1'],   128, 3, pad=1)
@@ -31,26 +33,32 @@ class Model(object):
         net['conv3_4'] = ConvLayer(net['conv3_3'], 256, 3, pad=1)
         net['pool3']   = PoolLayer(net['conv3_4'], 2, mode='average_exc_pad')
         net['conv4_1'] = ConvLayer(net['pool3'],   512, 3, pad=1)
-        net['conv4_2'] = ConvLayer(net['conv4_1'], 512, 3, pad=1)
-        # net['conv4_3'] = ConvLayer(net['conv4_2'], 512, 3, pad=1)
-        # net['conv4_4'] = ConvLayer(net['conv4_3'], 512, 3, pad=1)
-        # net['pool4']   = PoolLayer(net['conv4_4'], 2, mode='average_exc_pad')
-        # net['conv5_1'] = ConvLayer(net['pool4'],   512, 3, pad=1)
-        # net['conv5_2'] = ConvLayer(net['conv5_1'], 512, 3, pad=1)
-        # net['conv5_3'] = ConvLayer(net['conv5_2'], 512, 3, pad=1)
-        # net['conv5_4'] = ConvLayer(net['conv5_3'], 512, 3, pad=1)
-        # net['pool5']   = PoolLayer(net['conv5_4'], 2, mode='average_exc_pad')
+        net['main']    = ConvLayer(net['conv4_1'], 512, 3, pad=1)
+
+        # Secondary network for the semantic map.
+        net['map']   = InputLayer((1, 3, None, None))
+        net['map_3'] = PoolLayer(net['map'], 4, mode='average_exc_pad')
+        net['map_4'] = PoolLayer(net['map'], 8, mode='average_exc_pad')
+
+        net['sem3_1'] = ConcatLayer([net['conv3_1'], net['map_3']])
+        net['sem4_1'] = ConcatLayer([net['conv4_1'], net['map_4']])
+
         self.network = net
 
     def load_params(self): 
         vgg19_values = pickle.load(open('vgg19_conv.pkl', 'rb'))
-        output_layer = list(self.network.values())[-1]
-        params = lasagne.layers.get_all_param_values(output_layer)
-        lasagne.layers.set_all_param_values(output_layer, vgg19_values[:len(params)])
+        params = lasagne.layers.get_all_param_values(self.network['main'])
+        lasagne.layers.set_all_param_values(self.network['main'], vgg19_values[:len(params)])
 
-        self.tensor_input = T.tensor4()
-        self.tensor_outputs = lasagne.layers.get_output([self.network[l] for l in self.layers], self.tensor_input)
+        self.tensor_img = T.tensor4()
+        self.tensor_map = T.tensor4()
+        self.tensor_inputs = {self.network['img']: self.tensor_img, self.network['map']: self.tensor_map}
+        self.tensor_outputs = lasagne.layers.get_output([self.network[l] for l in self.layers], self.tensor_inputs)
 
 
 if __name__ == "__main__":
-    model = Model(layers=['conv3_1', 'conv4_1'])
+    model = Model(layers=['sem3_1'])
+
+    img = np.zeros((1, 3, 256, 256), dtype=np.float32)
+    map = np.zeros((1, 1, 256, 256), dtype=np.float32)
+    print(model.tensor_outputs[0].eval({model.tensor_img: img, model.tensor_map: map}).shape)

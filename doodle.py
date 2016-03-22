3bf9e722b29473e1d0f0e87b8bdcf78865b38bef
Alex J. Champandard <alexjc@aigamedev.com>
2016-03-22 20:50:45 +0100

    Work-around for #8, can't reproduce. Hardware issue?

--- doodle.py
+++ doodle.py
@@ -290,10 +290,19 @@ class NeuralGenerator(object):
         style_map = skimage.transform.rescale(self.style_map_original * args.semantic_weight, scale) * 255.0
         self.style_map = style_map.transpose((2, 0, 1))[np.newaxis].astype(np.float32)
 
+        # Workaround for Issue #8. Not clear what this is caused by, NaN seems to happen in convolution node
+        # on some OSX installations. https://github.com/alexjc/neural-doodle/issues/8
+        if args.safe_mode:
+            from theano.compile.nanguardmode import NanGuardMode
+            flags = {'mode': NanGuardMode(nan_is_error=True, inf_is_error=True, big_is_error=False)}
+        else:
+            flags = {}
+
         # Compile a function to run on the GPU to extract patches for all layers at once.
         extractor = theano.function(
                         [self.model.tensor_img, self.model.tensor_map],
-                        self.extract_patches([self.model.tensor_outputs['sem'+l] for l in self.style_layers]))
+                        self.extract_patches([self.model.tensor_outputs['sem'+l] for l in self.style_layers]),
+                        **flags)
         result = extractor(self.style_image, self.style_map)
 
         # For each layer, we now have a set of patches and their magnitude.
